42339b2733d629ac0cc4ca1eb8d6fe8af8d6a3a0
Gerardo Regalado <gerardo.regalado24@gmail.com>
2024-01-24 19:15:19 -0600

    Feat(): Created company page and page not found component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { CompanyPageComponent } from './company-page/company-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    CompanyPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path: '', redirectTo: '/company', pathMatch: 'full'},
+      {path: '**', component: PageNotFoundComponent},
+      {path: 'home', component: PageNotFoundComponent },
+      {path: 'company', component: CompanyPageComponent}
+    ])
   ],
   providers: [
     provideClientHydration()
