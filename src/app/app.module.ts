d6e0a5fb70216d10cec4cebada3c36e15df58c62
Gerardo Regalado <gerardo.regalado24@gmail.com>
2024-01-27 11:32:12 -0600

    Feat(): Developed home component adding an image, header and icons, also doing some modifications in order to make producst the default route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, bootstrapApplication, provideClientHydration } from '@angular/platform-browser';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { RouterModule } from '@angular/router';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { CompanyPageComponent } from './company-page/company-page.component';
 import { HomeComponent } from './home/home.component';
 
@@ -24,10 +23,8 @@ import { HomeComponent } from './home/home.component';
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot([
-      {path: 'home', component: HomeComponent },
       {path: 'company', component: CompanyPageComponent},
-      {path: '**', component: PageNotFoundComponent},
-      {path: '', redirectTo: '/home', pathMatch: 'full'},
+      {path: '', redirectTo: '/company', pathMatch: 'full'},
 
     ])
   ],
